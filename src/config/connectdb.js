6d8d050f415171b394a8d8a6e1f44b0b6e69cafa
rohitmangal4 <mangaleswaran2001@gmail.com>
2025-05-03 02:11:01 +0530

    resolved

--- src/config/connectdb.js
+++ src/config/connectdb.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 
 function connect(){
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.DATABASE_CLUSTER_ID)
 .then(()=>{
     console.log("MongoDB Connected......");   
 })
